6781d3cb4f3e64fb312b34c3d624320d12cd5f65
Ashwin <aswinrajesh1@gmail.com>
2021-01-18 19:42:23 +0530

    Added gnss

--- src/client.py
+++ src/client.py
@@ -28,32 +28,77 @@ sys.path.append("/HDD/CARLA_0.9.11/PythonAPI/carla/dist/carla-0.9.11-py3.7-linux
 
 import carla
 import numpy as np
+import time
 
-class client:
+class agent:
     def __init__(self):
         self.client = carla.Client('localhost', 2000)
-        self.world  = client.get_world()
-        self.bp_lib = world.get_blueprint_library()
+        self.world  = self.client.get_world()
+        self.bp_lib = self.world.get_blueprint_library()
 
-    def spawn_car(self):
-        car_bp = np.choice(self.bp_lib.filter("vehicle.*.*"))
-        car_tf = np.choice(self.world.get_map().get_spawn_points())
+        self.actor_list = []
 
-        self.car = self.world.spawn_actor(car_bp, car_tf)
+    def spawn_vehicle(self):
+        car_bp = np.random.choice(self.bp_lib.filter("vehicle.*.*"))
+        car_tf = np.random.choice(self.world.get_map().get_spawn_points())
 
-        print(" Spawning car at location : %d, %d, %d"%(car_tf.rotation.x, car_tf.rotation.y, car_tf.rotation.z))
+        self.vehicle = self.world.spawn_actor(car_bp, car_tf)
+        self.actor_list.append(self.vehicle)
+        self.vehicle.set_autopilot(True)
+
+        print(" Spawning vehicle at location : %d, %d, %d"%(car_tf.location.x, car_tf.location.y, car_tf.location.z))
+        print(" Vehicle model : %s %s"%(self.vehicle.type_id.split('.')[1], self.vehicle.type_id.split('.')[2]))
 
     def spawn_imu(self):
-        if(self.car == None):
+        if(self.vehicle == None):
             print(" Error : spawn car first")
             return
 
         imu_bp = self.bp_lib.find("sensor.other.imu")
+        imu_bp.set_attribute('sensor_tick', '0.1')
         imu_tf = carla.Transform(carla.Location(0,0,0), carla.Rotation(0,0,0))
 
-        self.imu = self.world.spawn_actor(imu_bp, imu_tf, attach_to=self.car)
+        self.imu = self.world.spawn_actor(imu_bp, imu_tf, attach_to=self.vehicle)
+        self.actor_list.append(self.imu)
 
         self.imu.listen(self.imu_listen)
 
+
     def imu_listen(self, data):
-        self.data = data
+        self.imu_data = data
+
+    def spawn_gnss(self):
+        if(self.vehicle == None):
+            print(" Error : spawn car first")
+            return
+
+        gnss_bp = self.bp_lib.find("sensor.other.gnss")
+        gnss_bp.set_attribute('sensor_tick', '0.1')
+        gnss_tf = carla.Transform(carla.Location(0,0,0), carla.Rotation(0,0,0))
+
+        self.gnss = self.world.spawn_actor(gnss_bp, gnss_tf, attach_to=self.vehicle)
+        self.actor_list.append(self.gnss)
+        self.gnss.listen(self.gnss_listen)
+
+        map_geo = self.world.get_map().transform_to_geolocation(carla.Location(0,0,0))
+        self.geo_centre = {'lat': map_geo.latitude, 'lon':map_geo.longitude, 'alt':map_geo.altitude}
+
+        self.geo_rad    = 6371.00
+
+    def gnss_listen(self, data):
+        self.gnss_data = data
+
+    def __del__(self):
+        for a in self.actor_list:
+            a.destroy()
+            self.actor_list.remove(a)
+
+if __name__ == "__main__":
+    a = agent()
+
+    a.spawn_car()
+    a.spawn_gnss()
+
+    time.sleep(10)
+
+    del a
